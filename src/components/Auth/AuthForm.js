4fd12545d7a06e485d0f971b20e27bb685f52fbb
Mustapha Mubarak <58697223+mako-commits@users.noreply.github.com>
2022-08-11 10:22:29 +0100

    feat: Seperate sign in amd sign up forms

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,127 +1,25 @@
-import { useState, useRef, useContext } from "react";
-import AuthContext from "../../store/auth-context";
 import classes from "./AuthForm.module.css";
-import { Link, useHistory } from "react-router-dom";
-import Form from "react-bootstrap/Form";
-import FloatingLabel from "react-bootstrap/FloatingLabel";
-const API_KEY = process.env.REACT_APP_API_KEY;
+import { Tab, Tabs } from "react-bootstrap";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+
 const AuthForm = () => {
-  const history = useHistory();
-  const emailInputRef = useRef();
-  const passwordInputRef = useRef();
-  const authCtx = useContext(AuthContext);
-  const [isLogin, setIsLogin] = useState(true);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
-  };
-
-  //Handle form submission
-  const submitHandler = (event) => {
-    event.preventDefault();
-
-    const enteredEmail = emailInputRef.current.value;
-    const enteredPassword = passwordInputRef.current.value;
-
-    //* Add extra validation
-    setIsLoading(true);
-
-    let url;
-
-    if (isLogin) {
-      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
-    } else {
-      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
-    }
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify({
-        email: enteredEmail,
-        password: enteredPassword,
-        returnSecureToken: true,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => {
-        setIsLoading(false);
-        if (res.ok) {
-          return res.json();
-        } else {
-          return res.json().then((data) => {
-            //show error modal
-            // console.log(data.error.message);
-            // let errorMessage = "Authentication failed";
-            if (data && data.error && data.error.message) {
-              alert(data.error.message);
-              setErrorMessage(data.error.message);
-            } else {
-              alert("Something went wrong");
-            }
-            throw new Error(data.error.message);
-          });
-        }
-      })
-      .then((data) => {
-        //transform
-        //get current time and add it expiresIn and multiply bt 1000 to convert to millisecond
-        const expirationTime = new Date(
-          new Date().getTime() + +data.expiresIn * 1000
-        );
-        authCtx.login(data.idToken, expirationTime.toISOString());
-        setErrorMessage("");
-        history.replace("/profile");
-      })
-      .catch((error) => {
-        // setErrorMessage(data.error.message);
-        alert(error.message);
-        setIsLoading(false);
-        // console.log(error.message);
-      });
-  };
-
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="name@example.com"
-            required
-            ref={emailInputRef}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="password"
-            required
-            ref={passwordInputRef}
-          />
-        </Form.Group>
-        <div className={classes.actions}>
-          {!isLoading && (
-            <button>{isLogin ? "Login" : "Create Account"}</button>
-          )}
-
-          {isLoading && <button>Loading...</button>}
-          <button
-            type="button"
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? "Create a new account" : "Login with existing account"}
-          </button>
-          {errorMessage && <p className={classes.error}>{errorMessage}</p>}
-        </div>
-        {isLogin && <Link to="/forgotten-password">Forgotten Password?</Link>}
-      </Form>
+      {/* <h1>{isLogin ? "Login" : "Sign Up"}</h1> */}
+      <Tabs
+        defaultActiveKey="signIn"
+        id="justify-tab-example"
+        className="mb-3"
+        justify
+      >
+        <Tab eventKey="signUp" title="Sign Up" className={classes.active}>
+          <SignUp />
+        </Tab>
+        <Tab eventKey="signIn" title="Sign In" className={classes.active}>
+          <SignIn />
+        </Tab>
+      </Tabs>
     </section>
   );
 };
---
+++ src/components/Auth/SignIn.js
@@ -0,0 +1,111 @@
+import React from "react";
+import { useState, useRef, useContext } from "react";
+import AuthContext from "../../store/auth-context";
+import { useHistory } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import classes from "./AuthForm.module.css";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const SignIn = () => {
+  const history = useHistory();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  //Handle form submission
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    //* Add extra validation
+    setIsLoading(true);
+
+    fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key= ${API_KEY}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            //show error modal
+            // console.log(data.error.message);
+            // let errorMessage = "Authentication failed";
+            if (data && data.error && data.error.message) {
+              alert(data.error.message);
+              setErrorMessage(data.error.message);
+            } else {
+              alert("Something went wrong");
+            }
+            throw new Error(data.error.message);
+          });
+        }
+      })
+      .then((data) => {
+        //transform
+        //get current time and add it expiresIn and multiply bt 1000 to convert to millisecond
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
+        authCtx.login(data.idToken, expirationTime.toISOString());
+        setErrorMessage("");
+        history.replace("/profile");
+      })
+      .catch((error) => {
+        // setErrorMessage(data.error.message);
+        alert(error.message);
+        setIsLoading(false);
+        // console.log(error.message);
+      });
+  };
+  return (
+    <>
+      <h3 className="mb-3">Sign in to your account.</h3>
+      <Form onSubmit={submitHandler}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Control
+            type="email"
+            placeholder="Email Address"
+            required
+            ref={emailInputRef}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            required
+            ref={passwordInputRef}
+          />
+        </Form.Group>
+        <div className={classes.actions}>
+          <Button variant="primary" type="submit">
+            {isLoading ? "Loading…" : "Sign In"}
+          </Button>
+          <Link to="/forgotten-password">Forgot your password?</Link>
+          {errorMessage && <p className={classes.error}>{errorMessage}</p>}
+        </div>
+      </Form>
+    </>
+  );
+};
+
+export default SignIn;
